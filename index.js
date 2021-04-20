13dbf38ec49c34d6cd7669bbb37436dd96aee67a
Evgeny Vlasenko <mahnunchik@gmail.com>
2021-04-20 17:36:18 +0300

    feat: wip load txs

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import monerolib from 'monerolib';
 import HDKey from 'hdkey';
 
+const TXIDS_CHUNK = 50;
+
 export default class MoneroWallet {
   #wallet;
   #cache;
+  #request;
+  #apiNode;
+  #apiWeb;
   #addressType;
   #isLocked;
+  #txs = [];
+
+  get #txIds() {
+    return this.#txs.map(item => item.txId);
+  }
 
   get decimals() {
     return 12;
@@ -36,6 +46,21 @@ export default class MoneroWallet {
     }
     this.#cache = options.cache;
 
+    if (!options.request) {
+      throw new TypeError('request should be passed');
+    }
+    this.#request = options.request;
+
+    if (!options.apiNode) {
+      throw new TypeError('apiNode should be passed');
+    }
+    this.#apiNode = options.apiNode;
+
+    if (!options.apiWeb) {
+      throw new TypeError('apiWeb should be passed');
+    }
+    this.#apiWeb = options.apiWeb;
+
     if (options.seed) {
       this.#wallet = this.#walletFromSeed(options.seed);
       this.#isLocked = false;
@@ -54,7 +79,7 @@ export default class MoneroWallet {
     const hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'));
     // https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
     // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-    const base = hdkey.derive("m / 44' / 128' / 0'");
+    const base = hdkey.derive("m/44'/128'/0'");
     return new monerolib.Wallet({
       seed: base.privateKey,
     });
@@ -80,6 +105,57 @@ export default class MoneroWallet {
     this.#isLocked = false;
   }
 
+  async load() {
+    const txIds = (await this.#cache.get('txIds')) || [];
+    const txs = await this.#loadTxs(txIds);
+    this.#txs = txs.map((tx) => this.#parseTx(tx));
+  }
+
+  async #loadTxs(txIds) {
+    const txs = [];
+    for (let i = 0; i < Math.ceil(txIds.length / TXIDS_CHUNK); i++) {
+      const ids = txIds.slice(i * TXIDS_CHUNK, i * TXIDS_CHUNK + TXIDS_CHUNK);
+      txs.push(await this.#request({
+        baseURL: this.#apiNode,
+        url: `api/v1/txs/${ids.join(',')}`,
+        method: 'get',
+        seed: 'public',
+      }));
+    }
+    return txs.flat();
+  }
+
+  #parseTx(tx) {
+    let valid = false;
+    for (const output of tx.outs) {
+      const derivation = monerolib.cryptoUtil.generateKeyDerivation(output.txPubKey, this.#wallet.secretViewKey);
+      for (const address of this.#getAllAddresses()) {
+        const pubKey = monerolib.cryptoUtil.derivePublicKey(derivation, output.index, address.publicSpendKey);
+        if (pubKey === output.targetKey) {
+          valid = true;
+          if (output.rctType !== monerolib.ringct.RCTTypes.Null) {
+            const rct = monerolib.ringct.decodeRct(
+              { amount: output.ecdhInfoAmount, mask: output.ecdhInfoMask },
+              output.outPk,
+              output.rctType,
+              output.index,
+              derivation
+            );
+            output.amount = rct.amount;
+          }
+        }
+      }
+    }
+    /*
+    for (const input of tx.ins) {
+    }
+    */
+    if (!valid) {
+      throw new Error('invalid transaction');
+    }
+    return tx;
+  }
+
   publicKey() {
     return JSON.stringify({
       publicSpendKey: this.#wallet.publicSpendKey.toString('hex'),
@@ -95,7 +171,23 @@ export default class MoneroWallet {
     }
   }
 
+  #getAllAddresses() {
+    return this.addressTypes.map((type) => this.#getAddress(type));
+  }
+
   getNextAddress() {
     return this.#getAddress(this.#addressType);
+  }
+
+  async addTx(txId) {
+    await this.addTxs([txId]);
+  }
+
+  async addTxs(txIds) {
+    // check if already added
+    txIds = txIds.filter(txId => !this.#txIds.includes(txId));
+    const txs = await this.#loadTxs(txIds);
+    this.#txs = this.#txs.concat(txs.map((tx) => this.#parseTx(tx)));
+    await this.#cache.set('txIds', this.#txIds);
   }
 }
