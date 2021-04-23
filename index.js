b34582971529c28510717f21ef0930fe515dd998
Evgeny Vlasenko <mahnunchik@gmail.com>
2021-04-23 21:19:12 +0300

    feat: load transactions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import monerolib from 'monerolib';
 import HDKey from 'hdkey';
+import BN from 'bn.js';
 
 const TXIDS_CHUNK = 50;
 
@@ -12,6 +13,7 @@ export default class MoneroWallet {
   #addressType;
   #isLocked;
   #txs = [];
+  #outputs = new Map();
 
   get #txIds() {
     return this.#txs.map(item => item.txId);
@@ -40,6 +42,16 @@ export default class MoneroWallet {
     return this.#isLocked;
   }
 
+  get #unspents() {
+    return Array.from(this.#outputs.values()).filter(item => item.spent !== true);
+  }
+
+  get balance() {
+    return this.#unspents
+      .reduce((balance, item) => balance.add(new BN(item.amount, 10)), new BN(0))
+      .toString(10);
+  }
+
   constructor(options = {}) {
     if (!options.cache) {
       throw new TypeError('cache should be passed');
@@ -61,12 +73,14 @@ export default class MoneroWallet {
     }
     this.#apiWeb = options.apiWeb;
 
+    const nettype = options.useTestNetwork ? 'regtest' : 'mainnet';
+
     if (options.seed) {
-      this.#wallet = this.#walletFromSeed(options.seed);
+      this.#wallet = this.#walletFromSeed(options.seed, nettype);
       this.#isLocked = false;
     } else if (options.publicKey) {
       const keys = JSON.parse(options.publicKey);
-      this.#wallet = this.#walletFromKeys(keys.publicSpendKey, keys.secretViewKey);
+      this.#wallet = this.#walletFromKeys(keys.publicSpendKey, keys.secretViewKey, nettype);
       this.#isLocked = true;
     } else {
       throw new TypeError('seed or publicKey should be passed');
@@ -75,33 +89,36 @@ export default class MoneroWallet {
     this.#addressType = options.addressType || this.addressTypes[0];
   }
 
-  #walletFromSeed(seed) {
+  #walletFromSeed(seed, nettype) {
     const hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'));
     // https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
     // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
     const base = hdkey.derive("m/44'/128'/0'");
     return new monerolib.Wallet({
       seed: base.privateKey,
+      nettype,
     });
   }
 
-  #walletFromKeys(publicSpendKey, secretViewKey) {
+  #walletFromKeys(publicSpendKey, secretViewKey, nettype) {
     return new monerolib.Wallet({
       publicSpendKey,
       secretViewKey,
+      nettype,
     });
   }
 
   lock() {
     this.#wallet = this.#walletFromKeys(
       this.#wallet.publicSpendKey,
-      this.#wallet.secretViewKey
+      this.#wallet.secretViewKey,
+      this.#wallet.nettype
     );
     this.#isLocked = true;
   }
 
   unlock(seed) {
-    this.#wallet = this.#walletFromSeed(seed);
+    this.#wallet = this.#walletFromSeed(seed, this.#wallet.nettype);
     this.#isLocked = false;
   }
 
@@ -109,6 +126,32 @@ export default class MoneroWallet {
     const txIds = (await this.#cache.get('txIds')) || [];
     const txs = await this.#loadTxs(txIds);
     this.#txs = txs.map((tx) => this.#parseTx(tx));
+
+    for (const tx of this.#txs) {
+      for (const output of tx.outs) {
+        if (output.ours) {
+          this.#outputs.set(`${tx.txId}:${output.index}`, {
+            ...output,
+            txId: tx.txId,
+            txPubKey: tx.txPubKey,
+          });
+        }
+      }
+      for (const input of tx.ins) {
+        for (const output of input.keyOutputs) {
+          if (output.ours && output.spent) {
+            const id = `${output.txId}:${output.index}`;
+            if (this.#outputs.has(id)) {
+              this.#outputs.get(id).spent = true;
+            } else {
+              this.#outputs.set(`${tx.txId}:${output.index}`, output);
+            }
+          }
+        }
+      }
+    }
+
+    await this.#cache.set('txIds', this.#txIds);
   }
 
   async #loadTxs(txIds) {
@@ -122,16 +165,19 @@ export default class MoneroWallet {
         seed: 'public',
       }));
     }
-    return txs.flat();
+    return txs.flat().sort((a, b) => a.time - b.time);
   }
 
   #parseTx(tx) {
     let valid = false;
     for (const output of tx.outs) {
-      const derivation = monerolib.cryptoUtil.generateKeyDerivation(output.txPubKey, this.#wallet.secretViewKey);
+      const derivation = monerolib.cryptoUtil.generateKeyDerivation(
+        Buffer.from(tx.txPubKey, 'hex'),
+        this.#wallet.secretViewKey
+      );
       for (const address of this.#getAllAddresses()) {
         const pubKey = monerolib.cryptoUtil.derivePublicKey(derivation, output.index, address.publicSpendKey);
-        if (pubKey === output.targetKey) {
+        if (pubKey.toString('hex') === output.targetKey) {
           valid = true;
           if (output.rctType !== monerolib.ringct.RCTTypes.Null) {
             const rct = monerolib.ringct.decodeRct(
@@ -142,16 +188,46 @@ export default class MoneroWallet {
               derivation
             );
             output.amount = rct.amount;
+            output.ours = true;
           }
         }
       }
     }
-    /*
+
     for (const input of tx.ins) {
+      for (const output of input.keyOutputs) {
+        const derivation = monerolib.cryptoUtil.generateKeyDerivation(
+          Buffer.from(output.txPubKey, 'hex'),
+          this.#wallet.secretViewKey
+        );
+        for (const address of this.#getAllAddresses()) {
+          const pubKey = monerolib.cryptoUtil.derivePublicKey(derivation, output.index, address.publicSpendKey);
+          if (pubKey.toString('hex') === output.targetKey) {
+            valid = true;
+            if (output.rctType !== monerolib.ringct.RCTTypes.Null) {
+              const rct = monerolib.ringct.decodeRct(
+                { amount: output.ecdhInfoAmount, mask: output.ecdhInfoMask },
+                output.outPk,
+                output.rctType,
+                output.index,
+                derivation
+              );
+              output.amount = rct.amount;
+              output.ours = true;
+            }
+            const secKey = monerolib.cryptoUtil.deriveSecretKey(derivation, output.index, address.secretSpendKey);
+            const keyImage = monerolib.cryptoUtil.generateKeyImage(pubKey, secKey);
+            if (keyImage.toString('hex') === input.keyImage) {
+              // spent
+              output.spent = true;
+            }
+          }
+        }
+      }
     }
-    */
+
     if (!valid) {
-      throw new Error('invalid transaction');
+      throw new Error('Not ours transaction');
     }
     return tx;
   }
@@ -165,9 +241,9 @@ export default class MoneroWallet {
 
   #getAddress(type) {
     if (type === 'address') {
-      return this.#wallet.getAddress().toString();
+      return this.#wallet.getAddress();
     } else if (type === 'subaddress') {
-      return this.#wallet.getSubaddress(0, 1).toString();
+      return this.#wallet.getSubaddress(0, 1);
     }
   }
 
@@ -176,7 +252,7 @@ export default class MoneroWallet {
   }
 
   getNextAddress() {
-    return this.#getAddress(this.#addressType);
+    return this.#getAddress(this.#addressType).toString();
   }
 
   async addTx(txId) {
